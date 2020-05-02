1dde4ddc0fd2ce6d8ee431013f92150959dc383e
LauraSw <56430448+LauraSw@users.noreply.github.com>
2020-05-02 15:58:21 +0200

    02

---
+++ L02_Sequenzmemory/Sequenzmemory.js
@@ -0,0 +1,8 @@
+"use strict";
+var Sequenzmemory;
+(function (Sequenzmemory) {
+    let input = inputEingabe;
+    input.length;
+    document.getElementById("TimeIsUp").innerHTML = "Time is up!";
+})(Sequenzmemory || (Sequenzmemory = {}));
+//# sourceMappingURL=Sequenzmemory.js.map
---
+++ L02_Sequenzmemory/Sequenzmemory.ts
@@ -0,0 +1,8 @@
+namespace Sequenzmemory {
+
+let input:string = inputEingabe;
+input.length
+
+document.getElementById("TimeIsUp").innerHTML= "Time is up!";
+
+}
